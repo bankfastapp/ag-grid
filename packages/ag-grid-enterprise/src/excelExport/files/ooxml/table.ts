ea15b1ccad3ee5478c2c734e642dd1347dad3eaf
Guilherme Lopes <guilherme@gwm-solutions.com>
2025-03-06 14:52:49 +0000

    AG-14317 - fixed line break as Excel Table Column name

--- packages/ag-grid-enterprise/src/excelExport/files/ooxml/table.ts
+++ packages/ag-grid-enterprise/src/excelExport/files/ooxml/table.ts
@@ -1,7 +1,7 @@
 import type { ExcelOOXMLTemplate } from 'ag-grid-community';
 
 import type { ExcelDataTable } from '../../assets/excelInterfaces';
-import { getExcelColumnName } from '../../assets/excelUtils';
+import { getExcelColumnName, satanizeTableName } from '../../assets/excelUtils';
 
 const tableFactory: ExcelOOXMLTemplate = {
     getTemplate(dataTable: ExcelDataTable, idx: number) {
@@ -76,7 +76,7 @@ const tableFactory: ExcelOOXMLTemplate = {
                         properties: {
                             rawMap: {
                                 id: (idx + 1).toString(),
-                                name: col,
+                                name: satanizeTableName(col),
                                 dataCellStyle: 'Normal',
                             },
                         },
